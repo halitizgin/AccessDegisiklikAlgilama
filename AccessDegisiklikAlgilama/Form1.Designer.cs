efaae779d75c44f66d9e7ede9f5c106f61254b14
Halit IZGIN <halit_izgin@outlook.com.tr>
2019-05-27 20:33:12 +0300

    Initial commit

---
+++ AccessDegisiklikAlgilama/Form1.Designer.cs
@@ -0,0 +1,184 @@
+﻿namespace AccessDegisiklikAlgilama
+{
+    partial class Form1
+    {
+        /// <summary>
+        ///Gerekli tasarımcı değişkeni.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///Kullanılan tüm kaynakları temizleyin.
+        /// </summary>
+        ///<param name="disposing">yönetilen kaynaklar dispose edilmeliyse doğru; aksi halde yanlış.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer üretilen kod
+
+        /// <summary>
+        /// Tasarımcı desteği için gerekli metot - bu metodun 
+        ///içeriğini kod düzenleyici ile değiştirmeyin.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.listView1 = new System.Windows.Forms.ListView();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.columnHeader1 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader2 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader3 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.columnHeader4 = ((System.Windows.Forms.ColumnHeader)(new System.Windows.Forms.ColumnHeader()));
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.SuspendLayout();
+            // 
+            // listView1
+            // 
+            this.listView1.Columns.AddRange(new System.Windows.Forms.ColumnHeader[] {
+            this.columnHeader1,
+            this.columnHeader2,
+            this.columnHeader3,
+            this.columnHeader4});
+            this.listView1.FullRowSelect = true;
+            this.listView1.Location = new System.Drawing.Point(12, 12);
+            this.listView1.Name = "listView1";
+            this.listView1.Size = new System.Drawing.Size(240, 313);
+            this.listView1.TabIndex = 0;
+            this.listView1.UseCompatibleStateImageBehavior = false;
+            this.listView1.View = System.Windows.Forms.View.Details;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(258, 36);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(41, 13);
+            this.label1.TabIndex = 1;
+            this.label1.Text = "Kişi Adı";
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(344, 33);
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 20);
+            this.textBox1.TabIndex = 2;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(258, 62);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(58, 13);
+            this.label2.TabIndex = 1;
+            this.label2.Text = "Kişi Soyadı";
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(344, 59);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 20);
+            this.textBox2.TabIndex = 2;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(258, 88);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(62, 13);
+            this.label3.TabIndex = 1;
+            this.label3.Text = "Kişi Telefon";
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(344, 85);
+            this.textBox3.MaxLength = 10;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 20);
+            this.textBox3.TabIndex = 2;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(356, 111);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 23);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Ekle";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.Button1_Click);
+            // 
+            // columnHeader1
+            // 
+            this.columnHeader1.Text = " #";
+            this.columnHeader1.Width = 36;
+            // 
+            // columnHeader2
+            // 
+            this.columnHeader2.Text = "Kişi Adı";
+            // 
+            // columnHeader3
+            // 
+            this.columnHeader3.Text = "Kişi Soyadı";
+            this.columnHeader3.Width = 70;
+            // 
+            // columnHeader4
+            // 
+            this.columnHeader4.Text = "Kişi Telefon";
+            this.columnHeader4.Width = 71;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 1000;
+            this.timer1.Tick += new System.EventHandler(this.Timer1_Tick);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(456, 335);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.listView1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.FormClosing += new System.Windows.Forms.FormClosingEventHandler(this.Form1_FormClosing);
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView listView1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.ColumnHeader columnHeader1;
+        private System.Windows.Forms.ColumnHeader columnHeader2;
+        private System.Windows.Forms.ColumnHeader columnHeader3;
+        private System.Windows.Forms.ColumnHeader columnHeader4;
+        private System.Windows.Forms.Timer timer1;
+    }
+}
+
---
+++ AccessDegisiklikAlgilama/Form1.cs
@@ -0,0 +1,85 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+using System.Data.OleDb;
+using System.IO;
+
+namespace AccessDegisiklikAlgilama
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        public OleDbConnection con;
+        DateTime SonErisim;
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+            SonErisim = DateTime.Now;
+            con = new OleDbConnection("Provider=Microsoft.ACE.OLEDB.12.0; Data Source=Kisiler.accdb");
+            KisileriGetir();
+            timer1.Start();
+        }
+
+        private void KisileriGetir()
+        {
+            listView1.Items.Clear();
+            con.Open();
+            OleDbCommand komut = new OleDbCommand("SELECT * FROM Kisiler", con);
+            OleDbDataReader okuyucu = komut.ExecuteReader();
+            while (okuyucu.Read())
+            {
+                string id = okuyucu["ID"].ToString();
+                string adi = okuyucu["Kisi_Adi"].ToString();
+                string soyadi = okuyucu["Kisi_Soyadi"].ToString();
+                string telefon = okuyucu["Kisi_Telefon"].ToString();
+                int count = listView1.Items.Count;
+                listView1.Items.Add(id);
+                listView1.Items[count].SubItems.Add(adi);
+                listView1.Items[count].SubItems.Add(soyadi);
+                listView1.Items[count].SubItems.Add(telefon);
+            }
+            con.Close();
+        }
+
+        private void Button1_Click(object sender, EventArgs e)
+        {
+            con.Open();
+            string adi = textBox1.Text.Trim();
+            string soyadi = textBox2.Text.Trim();
+            string telefon = textBox3.Text.Trim();
+            OleDbCommand komut = new OleDbCommand("INSERT INTO Kisiler (Kisi_Adi, Kisi_Soyadi, Kisi_Telefon) VALUES(@adi,@soyadi,@telefon)", con);
+            komut.Parameters.AddWithValue("@adi", adi);
+            komut.Parameters.AddWithValue("@soyadi", soyadi);
+            komut.Parameters.AddWithValue("@telefon", telefon);
+            komut.ExecuteNonQuery();
+            con.Close();
+        }
+
+        private void Form1_FormClosing(object sender, FormClosingEventArgs e)
+        {
+            
+        }
+
+        private void Timer1_Tick(object sender, EventArgs e)
+        {
+            FileInfo info = new FileInfo("Kisiler.accdb");
+            DateTime erisim = info.LastWriteTime;
+            int compare = DateTime.Compare(SonErisim, erisim);
+            if (compare < 0)
+            {
+                KisileriGetir();
+                SonErisim = erisim;
+            }
+        }
+    }
+}
